e373b475a7938266ccdac08315ba9f31554a02db
Madhav <madhav5448@gmail.com>
2022-08-14 19:44:50 +0530

    add page no page found

---
+++ src/component/NoPageFound/NoPageFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const NoPageFound = () => {
+  return (
+    <div className='nopagefound'><h1>NoPageFound</h1>
+    <h3>Click on the logo for home</h3>
+ 
+    </div>
+  )
+}
+
+export default NoPageFound
